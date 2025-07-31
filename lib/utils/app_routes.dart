4c634cfe4952a18d34c6adececa73c2f6c28a36a
AhmedMostafa106 <ahmed1062002@gmail.com>
2025-07-31 17:06:30 +0300

    add some packages

---
+++ lib/utils/app_routes.dart
@@ -0,0 +1,6 @@
+class AppRoutes {
+  static const String homeScreen = 'HomeScreen';
+  static const String splashScreen = 'SplashScreen';
+  static const String chatsScreen = 'ChatsScreen';
+  static const String settingsScreen = 'SettingsScreen';
+}

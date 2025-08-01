14f81a08d65b99d42cc38a67ca9bee2032544105
AhmedMostafa106 <ahmed1062002@gmail.com>
2025-08-01 11:18:35 +0300

    add pages

---
+++ lib/presentation/pages/conversation_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ConversationPage extends StatelessWidget {
+  const ConversationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: Text('Conversation Page')));
+  }
+}
